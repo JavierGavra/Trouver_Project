3ba784717943a588f2bedf3d4dbf7c568dced5fe
JavierGavra <javiergavra01@gmail.com>
2022-09-28 08:54:33 +0700

    [JG] Tahap Awal

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:trouver_project/pages/detail_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xff242A32),
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.white,
+              fontFamily: 'Poppins',
+            ),
+      ),
+      home: DetailPage(),
+    );
+  }
+}
